b437b81b8d6fe6b97d38ee29589232304b4ac1b4
mrizqi130 <rizqianshari3506@gmail.com>
2026-02-03 21:20:43 +0800

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Muhammad Rizqi Anshari";
+        System.out.println(name);
+
+        int age = 20;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Anshari";
+//        name = 100;
+        System.out.println(name);
+
+        var firstName ="Muhammad";
+        var middleName ="Rizqi";
+        var lastName ="Anshari";
+
+//        var company; error
+//        company = "Telkom Indonesia";
+        final String app = "Belajar Java";
+//        app = "Belajar php"; error krn sudah final
+    }
+}
