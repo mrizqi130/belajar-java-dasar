8fb7e3ebefd85f31ba188cc47653c1d1ec0049da
mrizqi130 <rizqianshari3506@gmail.com>
2026-02-03 20:51:24 +0800

    TipeDataChar

---
+++ src/TipeDataChar.java
@@ -0,0 +1,16 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char r = 'R';
+        char i = 'I';
+        char z = 'Z';
+        char q = 'Q';
+
+        System.out.print(r);
+        System.out.print(i);
+        System.out.print(z);
+        System.out.print(q);
+
+
+
+    }
+}
