ff55ff07e3d5701cad14ef5bb1cc5f226e756970
mrizqi130 <rizqianshari3506@gmail.com>
2026-02-03 20:30:09 +0800

    TipeDataNumber

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,24 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 1000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexInt = 0x1239;
+        int binInt = 0b1111100;
+
+        long balance = 1_2_3_4_245_000000000000L;
+        int sum = 1_000_000;
+
+//        widening casting : byte > short > int > long > float > double
+//        narrowing casting : double > float > long > int > char > short > byte
+
+
+    }
+}
