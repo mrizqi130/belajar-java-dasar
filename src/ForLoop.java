0b8f6f31b493768f75e4051eac32d0a246efcf00
mrizqi130 <rizqianshari3506@gmail.com>
2026-02-12 19:32:26 +0800

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+    public static void main(String[] args) {
+//        for (;;){
+//            System.out.println("unlimited");
+//        }
+//        var counter = 1;
+
+//        for (;counter<=10;){
+//            System.out.println("Perulangan "+counter);
+//            counter++;
+//        }
+        for (var counter = 1;counter<=11;counter++){
+            System.out.println("Perulangan "+counter);
+        }
+    }
+}
